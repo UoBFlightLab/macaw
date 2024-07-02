7afbb3c4af2437442e6802ce924ce4d09c65eeeb
Arthur <arthur.richards@bristol.ac.uk>
2024-07-02 06:33:37 +0100

    Improved mavlink subscription and data request

--- macaw/macaw.py
+++ macaw/macaw.py
@@ -23,20 +23,20 @@ class Macaw(Node):
         self.add_ros_publisher(UInt8,'status')
         self.add_ros_publisher(Bool,'is_armed')
         self.add_ros_publisher(NavSatFix,'gps')
-        # set up inbound MAVlink subscribers
-        self.mav_subscribers = {'HEARTBEAT': self.mav_heartbeat_callback,
-                                'GPS_RAW_INT': self.mav_gps_callback}
-        self.mav_messages = list(self.mav_subscribers.keys())
-        self.get_logger().info(f'Ready for MAVlink messages: {self.mav_messages}')
         # connect MAVlink
         self.declare_parameter('mavlink_connect_str','tcp:127.0.0.1:5760')
         connect_str = self.get_parameter('mavlink_connect_str')
         self.get_logger().info(f'Connecting to {connect_str.value}')
         self.mav = mavutil.mavlink_connection(connect_str.value)
+        # set up inbound MAVlink subscribers
+        self.mav_subscribers = {}
+        self.add_mav_subscriber('HEARTBEAT', self.mav_heartbeat_callback)
+        self.add_mav_subscriber('GPS_RAW_INT', self.mav_gps_callback, interval=1e6)
+        self.mav_messages = list(self.mav_subscribers.keys())
+        self.get_logger().info(f'Ready for MAVlink messages: {self.mav_messages}')
         # timer for inbound MAVlink handling
         timer_period = 0.001  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.request_mav_data()
         # ROS subscribers
         self.add_ros_subscriber(Empty,'arm',self.ros_arm_callback)
 
@@ -49,6 +49,18 @@ class Macaw(Node):
         topic_root = f'macaw/sysid{self.sysid}/'
         new_sub = self.create_subscription(ros_type,topic_root + topic,callback,10)
 
+    def add_mav_subscriber(self,mav_type,callback,interval=None):
+        mav_type_num = getattr(mavutil.mavlink,f'MAVLINK_MSG_ID_{mav_type}')
+        self.mav_subscribers[mav_type] = callback
+        if interval:
+            self.mav.mav.command_long_send(self.sysid,
+                                        1,
+                                        mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL,
+                                        0,
+                                        mav_type_num,
+                                        1e6,
+                                        0,0,0,0,0,0)
+
     def timer_callback(self):
         # process inbound MAVlink messages
         mav_msg = self.mav.recv_match(type=self.mav_messages, blocking=False)
@@ -58,15 +70,6 @@ class Macaw(Node):
             self.get_logger().info(f'Got {mav_msg_type} from {mav_msg_sender}')
             if mav_msg_sender==self.sysid:
                 self.mav_subscribers[mav_msg_type](mav_msg)
-
-    def request_mav_data(self):
-        self.mav.mav.command_long_send(self.sysid,
-                                       1,
-                                       mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL,
-                                       0,
-                                       24, #GPS_RAW_INT
-                                       1e6,
-                                       0,0,0,0,0,0)
 
     def mav_heartbeat_callback(self,mav_msg):
         ros_msg = UInt8()
